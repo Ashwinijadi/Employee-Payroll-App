806c1ea64301ad766efbe0c01d9937ac11d2934c
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-18 22:57:30 -0800

    added : Setters and getter are added for validation

---
+++ js/NewEmpPayroll.js
@@ -0,0 +1,68 @@
+
+class EmployeePayrollData {
+  get id() {
+    return this._id;
+  }
+  set id(id) {
+    this._id = id;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if (nameRegex.test(name))
+      this._name = name;
+    else throw "name is incorrect";
+  }
+  get profile() {
+    return this._profile;
+  }
+  set profile(profile) {
+    this._profile = profile;
+  }
+
+  get gender() {
+    return this._gender;
+  }
+  set gender(gender) {
+    this._gender = gender;
+  }
+
+  get department() {
+    return this._department;
+  }
+  set department(department) {
+    this._department = department;
+  }
+
+  get salary() {
+    return this._salary;
+  }
+  set salary(salary) {
+    this._salary = salary;
+  }
+
+  get note() {
+    return this._note;
+  }
+  set note(note) {
+    this._note = note;
+  }
+
+  get startDate() {
+    return this._startDate;
+  }
+  set startDate(startDate) {
+    if (startDate<=new Date() || startDate == undefined)
+      this._startDate = startDate;
+    else throw "Incorrect Date";
+  }
+
+  toString() {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+    return "id = " + this.id + " , name : " + this.name + ",gender=" + this.gender + "profile=" + this.profile + ",department=" + this.department + " , salary : " + this.salary + "," +
+      + ", date = " + empDate + ", note" + note;
+  }
+}
