59082a1a47778914cdc992222c7d5a9c45b3b387
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-11-19 21:48:42 -0800

    added : Ability to display Employee Payroll details in a tabular format from JS File using Template Literals

--- js/newHome.js
+++ js/newHome.js
@@ -3,22 +3,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const createInnerHtml = () => {
-    const innerHtml = `
-    <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Gender</th>
-        <th>Department</th>
-        <th>Salary</th>
-        <th>Start Date</th>
-        <th>Actions</th>
-    </tr>
+    const headerHtml=" <th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>"
+    const innerHtml = `${headerHtml}
     <tr>
         <td><img class="profile" alt="" src="/assets/Ellipse -1.png"></td>
         <td>Ashwini</td>
         <td>Female</td>
         <td>
-            <div class='dept-label'>HR</div>
+            <div class='dept-label'>Engineering</div>
             <div class='dept-label'>Finance</div>
         </td>
         <td>600000</td>
